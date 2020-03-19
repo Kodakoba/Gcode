ebe2babb13bbc9b5a990a27126c5784e4f567b26
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-03-19 16:14:54 +0700

    wordwrap doesn't dash spaces

--- addons/just_lib_it_up/lua/lib_it_up/extensions/String.lua
+++ addons/just_lib_it_up/lua/lib_it_up/extensions/String.lua
@@ -151,13 +151,16 @@ local function WrapByLetters(txt, curwid, fullwid, wids, line)
 	local ret = ""
 
 	local line = line or 0
+	local wrapped = false
 
 	for i, code in utf8.codes(txt) do 
 		local char = utf8.char(code)
 		local charw = (surface.GetTextSize(char))
 
 		if charw > curwid then 
-			ret = ret .. "-\n" .. char
+
+			local shoulddash = ret:sub(#ret):match("[^%s%c]") --not a space or control char; need to dash it
+			ret = ret .. (shoulddash and "-" or "") .. "\n" .. char
 
 			if wids then 
 				fullwid = wids[line + 1] or wids[#wids]
@@ -165,14 +168,14 @@ local function WrapByLetters(txt, curwid, fullwid, wids, line)
 			end
 
 			curwid = fullwid - charw
-
+			wrapped = true
 		else 
 			ret = ret .. char 
 			curwid = curwid - charw 
 		end
 	end
 
-	return ret, (fullwid - curwid), line
+	return ret, (fullwid - curwid), line, wrapped
 end
 
 local function WrapWord(word, curwid, fullwid, widtbl, line)
@@ -197,7 +200,7 @@ local function WrapWord(word, curwid, fullwid, widtbl, line)
 
 			line = line + 1
 		else 
-			local newtx, newwid, lines = WrapByLetters(word, fullwid - curwid, fullwid, widtbl, line)
+			local newtx, newwid, lines, didwrap = WrapByLetters(word, fullwid - curwid, fullwid, widtbl, line)
 			--if widtbl was provided, WrapByLetters'll figure out what to do
 
 			--lines is the amount of times the word has wrapped
@@ -205,17 +208,20 @@ local function WrapWord(word, curwid, fullwid, widtbl, line)
 			ret = ret .. newtx
 			curwid = newwid
 			line = lines
+			wrapped = wrapped or didwrap
 		end
 	else 
 		ret = ret .. word
 		curwid = curwid + tw
 	end
 
-	return ret, curwid, line
+	return ret, curwid, line, wrapped
 end
 
 function string.WordWrap2(txt, wid, font)
 	surface.SetFont(font or "RL24")
+
+	local wrapped = false 
 
 	if istable(wid) then 
 
@@ -227,24 +233,26 @@ function string.WordWrap2(txt, wid, font)
 
 		for word in string.gmatch(txt, "(.-)%s") do 
 			
-			local r2, w2, lines = WrapWord(word .. " ", curwid, nil, wid, line)
+			local r2, w2, lines, didwrap = WrapWord(word .. " ", curwid, nil, wid, line)
 			ret = ret .. r2
 			curwid = w2
 
 			line = lines
-
+			wrapped = wrapped or didwrap
 		end
 
 		local lastword = txt:match("[^%s]+$")
 
 		if lastword then
-			local r2, w2 = WrapWord(lastword, curwid, nil, wid, line)
+			local r2, w2, lines, didwrap = WrapWord(lastword, curwid, nil, wid, line)
 
 			ret = ret .. r2
 			curwid = w2
+			wrapped = wrapped or didwrap
+			print("wrapped last", word, "new width", curwid)
 		end
 
-		return ret, curwid
+		return ret, curwid, wrapped
 	else 
 		local widths = {}
 		local ret = ""
@@ -254,22 +262,23 @@ function string.WordWrap2(txt, wid, font)
 
 		for word in string.gmatch(txt, "(.-)%s") do 
 			
-			local r2, w2, wrapped = WrapWord(word .. " ", curwid, needwid)
+			local r2, w2, didwrap = WrapWord(word .. " ", curwid, needwid)
 			ret = ret .. r2
 			curwid = w2
-
+			wrapped = wrapped or didwrap
 		end
 
 		local lastword = txt:match("[^%s]+$")
 
 		if lastword then
-			local r2, w2 = WrapWord(lastword, curwid, needwid)
+			local r2, w2, didwrap = WrapWord(lastword, curwid, needwid)
 
 			ret = ret .. r2
 			curwid = w2
+			wrapped = wrapped or didwrap
 		end
 
-		return ret, curwid
+		return ret, curwid, wrapped
 		
 	end
 
@@ -337,6 +346,181 @@ function string.IsVowel(char)
 	return (not not vowels[string.lower(char)])
 end
 
+--[[
+	Takes a string and a table of shortcuts: for every shortcut ("string :shortcut:"),
+	attempts to replace it with table[shortcut]
+]]
+function string.Shortcut(str, shcuts)
+
+	for s1 in string.gmatch(str, ":(.-):") do --shortcuts, then tags 
+		if shcuts[s1] then 
+			str = str:gsub((":%s:"):format(s1), shcuts[s1], 1)
+		end
+	end
+
+	return str
+end
+
+function string.ParseTags(str, tags, shcuts)
+
+	local tags = {}
+	
+	local prevtagwhere
+
+	if shcuts then str = str:Shortcut(shcuts) end
+
+	for s1 in string.gmatch( str, "%b<>" ) do
+		local tagcont = s1:GetBetween("<>")
+
+		if not tagcont then continue end
+
+		local starts = str:find(s1, 1, true)
+
+		local tag, argsstr = tagcont:match(tagptrn)
+
+		local chTag = chathud.TagTable[tag]
+
+		if not chTag then 
+			local isend = tagcont:match(tagendptrn)
+			if not isend or not chathud.TagTable[isend] then print("no such tag:", tag, isend) continue end
+
+			for k,v in ipairs(table.Reverse(tags)) do
+				if not istable(v) then continue end  
+				if v.tag == isend and not v.ends and not v.ender then 
+					--create an ender tag, which will disable tag at k
+					v.ends = starts 
+					str = str:gsub(s1:PatternSafe(), "", 1)
+
+					local key = #tags + 1
+
+					if prevtagwhere then 
+						tags[key] = str:sub(prevtagwhere, starts+utflen(str)-2)	--if ender, put text first ender later
+						key = key + 1
+					end
+
+					tags[key] = {
+						tag = isend, 
+						ender = true, 
+						ends = v.realkey,	--ends tag with key v.realkey
+						realkey = key
+					}
+					
+					prevtagwhere = starts--+1
+
+					break
+				end 
+			end
+			continue
+		end
+
+		local info = {
+			SendTime = CurTime(),
+		}
+
+		if not prevtagwhere then 
+			tags[#tags + 1] = str:sub(1, starts-utflen(str))
+		end
+
+		local args = {}
+
+		for argtmp in string.gmatch(argsstr, ".-,") do 
+			local arg = argtmp:match(spacearg)
+
+			argsstr = argsstr:gsub(argtmp:PatternSafe(), "", 1)
+
+			local exp = arg:match("%[(.+)%]") 
+
+			if exp then 
+
+				local func = CompileExpression(exp, info)
+
+				if isstring(func) then 
+					print("Expression error: " .. func)
+					continue
+				end 
+
+				args[#args + 1] = func 
+				continue
+			end
+
+			local num = #args + 1
+
+			if not chTag.args[num] then break end 
+
+			local typ = chTag.args[num].type
+			if not chathud.TagTypes[typ] then print("Unknown argument type! ", typ) break end 
+
+			local ret = chathud.TagTypes[typ](arg)	
+
+			if ret then args[#args + 1] = ret end --if conversion to type succeeded
+		end 
+
+		local key = #tags + 1
+
+		local lastarg = argsstr:match(lastarg)
+
+		if lastarg then  
+
+			local exp = lastarg:match("%[(.+)%]") 
+
+			if exp then 
+
+				local func = CompileExpression(exp, info)
+
+				if isstring(func) then 
+					print("Expression error: " .. func)
+				end 
+
+				args[#args + 1] = func 
+				
+			else
+				args[#args + 1] = lastarg 
+			end
+
+		end
+
+		str = str:gsub(s1:PatternSafe(), "", 1)
+
+		if prevtagwhere then 
+
+			tags[key] = str:sub(prevtagwhere+utflen(str)-1, starts-1)
+			key = key + 1
+
+		end
+
+
+		for k,v in pairs(chTag.args) do 
+			if isnumber(args[k]) then
+				if v.min then 
+					args[k] = math.max(args[k], v.min)
+				end 
+				if v.max then 
+					args[k] = math.min(args[k], v.max)
+				end
+			end
+
+			if not args[k] then 
+				args[k] = v.default 
+			end 
+
+		end
+
+		tags[key] = {
+			tag = tag, 
+			args = args,
+			starts = starts,
+			realkey = key --for ender to keep track due to table reversing
+		}
+
+		prevtagwhere = starts
+	end
+
+	tags[#tags + 1] = string.sub(str, (prevtagwhere and prevtagwhere+utflen(str)-1) or 1, #str)
+
+
+	return str, tags
+end
+
 function printf(s, ...)
 	print(s:format(...))
 end
@@ -344,3 +528,4 @@ end
 function errorf(s, ...)
 	return error(s:format(...))
 end
+
