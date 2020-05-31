159664f6940f109aeb0f70dcf1ab243057100e0c
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-05-31 11:20:54 +0700

    more scaling for darkhud

--- addons/darkhud/lua/darkhud/cl_darkhud.lua
+++ addons/darkhud/lua/darkhud/cl_darkhud.lua
@@ -1,5 +1,4 @@
-local scale = 0.85
-local hsc = 0.85
+setfenv(1, _G)
 
 DarkHUD = DarkHUD or {}
 local wasvalid = false
@@ -9,28 +8,97 @@ local surface = surface
 
 if DarkHUD.Essentials then DarkHUD.Essentials:Remove() DarkHUD.Essentials = nil wasvalid = true end
 
-DarkHUD.EverUsed = false
 
-sql.Query("CREATE TABLE IF NOT EXISTS DarkHUD(used TEXT, settings TEXT)")
-DarkHUD.Used = DarkHUD.Used or sql.Query("SELECT used FROM DarkHUD")
-local used = DarkHUD.Used
 
-if not used then used = {} sql.Query("INSERT INTO DarkHUD(used, settings) VALUES('[]', '[]')") end
-if used and used[1] and used[1].used then used = util.JSONToTable(used[1].used) end
+--[[------------------------------]]
+--	  		Fonts setup
+--[[------------------------------]]
 
-function DarkHUD.SetUsed(key, val)
-	used[key] = val
+	DarkHUD.Fonts = DarkHUD.Fonts or {}
 
-	local str = SQLStr(util.TableToJSON(used))
-	local q = ("UPDATE DarkHUD SET used = %s"):format(str)
-	sql.Query(q)
-end
+	local fonts = DarkHUD.Fonts
+	fonts.NameFont = "Open Sans Semibold"
+	fonts.FactionFont = "Open Sans"
+
+	fonts.MoneyFont = "Open Sans"
+
+	fonts.VitalsNumberFont = "Open Sans"
+
+--[[------------------------------]]
+--	  	Used/not used setup
+--[[------------------------------]]
+
+	DarkHUD.EverUsed = false
+
+	sql.Query("CREATE TABLE IF NOT EXISTS DarkHUD(used TEXT, settings TEXT)")
+	DarkHUD.Used = DarkHUD.Used or sql.Query("SELECT used FROM DarkHUD")
+	local used = DarkHUD.Used
+
+	if not used then used = {} sql.Query("INSERT INTO DarkHUD(used, settings) VALUES('[]', '[]')") end
+	if used and used[1] and used[1].used then used = util.JSONToTable(used[1].used) end
+
+	function DarkHUD.SetUsed(key, val)
+		used[key] = val
+
+		local str = SQLStr(util.TableToJSON(used))
+		local q = ("UPDATE DarkHUD SET used = %s"):format(str)
+		sql.Query(q)
+	end
 
 
 local dh = DarkHUD
 
-dh.PaddingX = scale * 64
-dh.PaddingY = hsc * 48
+--[[------------------------------]]
+--	  Scaling & Rescaling setup
+--[[------------------------------]]
+
+	local scale = 0
+	local hsc = 0
+
+	function DarkHUD.ReScale()
+		print("changed to", ScrW(), ScrH())
+		scale = ScrH() / 1080 * 0.9
+		hsc = scale
+
+
+		fonts.NameHeight = 40 * scale
+		fonts.FactionHeight = 16 + 12 * scale
+		fonts.MoneyHeight = 28 * scale
+		fonts.VitalsNumberHeight = 12 + 16 * scale
+
+		surface.CreateFont("DarkHUD_Name", {
+			font = fonts.NameFont,
+			size = fonts.NameHeight
+		})
+
+		surface.CreateFont("DarkHUD_Faction", {
+			font = fonts.FactionFont,
+			size = fonts.FactionHeight
+		})
+
+		surface.CreateFont("DarkHUD_Money", {
+			font = fonts.MoneyFont,
+			size = fonts.MoneyHeight
+		})
+
+		surface.CreateFont("DarkHUD_VitalsNumber", {
+			font = fonts.VitalsNumberFont,
+			size = fonts.VitalsNumberHeight
+		})
+
+		dh.PaddingX = 16 + scale * 36
+		dh.PaddingY = 16 + hsc * 24
+
+		local f = DarkHUD.Essentials
+		if not IsValid(f) then return end
+
+		f:ResizeElements()
+	end
+
+	DarkHUD.ReScale()
+
+	hook.Add("OnScreenSizeChanged", "DarkHUD_Scale", DarkHUD.ReScale)
+
 
 local tex_corner8	= surface.GetTextureID( "gui/corner8" )
 local tex_corner16	= surface.GetTextureID( "gui/corner16" )
@@ -95,14 +163,6 @@ function draw.RoundedBoxExEx( bordersize, x, y, w, h, color, btl, btr, bbl, bbr 
 
 end
 
-surface.CreateFont("FactionFont", {
-    font = "Open Sans",
-    size = 24,
-    weight = 200
-})
-
-local padx, pady = dh.PaddingX, dh.PaddingY
-
 function DarkHUD.Create()
 	if DarkHUD.Essentials then DarkHUD.Essentials:Remove() DarkHUD.Essentials = nil end
 	DarkHUD.Essentials = vgui.Create("FFrame")
@@ -118,7 +178,7 @@ function DarkHUD.Create()
 	f:SetSize(scale*500, hsc*200)
 	local fw, fh = f:GetSize()
 
-	f:SetPos(padx, ScrH() - fh - pady)
+	f:SetPos(dh.PaddingX, ScrH() - fh - dh.PaddingY)
 	f:SetCloseable(false, true)
 
 	f.BackgroundColor.a = 255
@@ -128,52 +188,67 @@ function DarkHUD.Create()
 
 	local vls = f.Vitals
 
-	vls:SetSize(f:GetWide(), f:GetTall() - hs)
-	vls:SetPos(0, hs)
+	vls:SetSize(f:GetWide(), f:GetTall() - hs - 12 - 64*scale)
+	vls:SetPos(0, hs + 12 + 64*scale)
 
 
 	f.Economy = vgui.Create("InvisFrame", f)
 	local ecn = f.Economy
 
-	ecn:SetSize(f:GetWide(), f:GetTall() - hs)
-	ecn:SetPos(0, hs)
+	ecn:SetSize(f:GetWide(), f:GetTall() - hs - 12 - 64*scale)
+	ecn:SetPos(0, hs + 12 + 64*scale)
 
+	ecn:MoveToBefore(vls)   --draw economy behind vitals so when you press C the EXP box doesn't show
+							--it does some alpha trickery to look better
 	ecn:SetAlpha(0)
 
 
 	local av = vgui.Create("AvatarImage", f)
 	f.Avatar = av
 
-	av:SetSize(64, 64)
+	av:SetSize(64 * scale, 64 * scale)
 	av:SetPos(16, hs + 8)
-	av:SetPlayer(LocalPlayer(), 128)
+	av:SetPlayer(LocalPlayer(), 64)
 
 	av:SetPaintedManually(true)
+
+	function f:ResizeElements()
+
+		self:SetSize(scale*500, hsc*200)
+		self:SetPos(dh.PaddingX, ScrH() - fh - dh.PaddingY)
+
+		vls:SetSize(f:GetWide(), f:GetTall() - hs - 12 - 64*scale)
+		vls:SetPos(0, hs + 12 + 64*scale)
+
+		ecn:SetSize(f:GetWide(), f:GetTall() - hs - 12 - 64*scale)
+		ecn:SetPos(0, hs + 12 + 64*scale)
+
+		av:SetSize(64 * scale, 64 * scale)
+		av:SetPos(16, hs + 8)
+
+	end
 
 	local tcol = Color(100, 100, 100)
 
 	f.Shadow = {spread = 0.8, intensity = 4}
 
-	local hpfr = 0
-	local arfr = 0
-
-	local contextopen = false
-
-	local pl, pm, pe = LocalPlayer():GetLevel(), LocalPlayer():GetMoney()
+	local pl, pm = LocalPlayer():GetLevel(), LocalPlayer():GetMoney()
 
 	local PopupLevel = 0
 	local PopupMoney = 0
 
-	local pld, pmd, ped = {}, {}, {} --differences
+	local pmd  = {} --differences
 
 	local mCol = Color(250, 250, 250)
 	local lvCol = Color(250, 250, 250)
 
 	local boxcol = Color(50, 50, 50, 253)
 
+	local me = LocalPlayer()
+
 	function f:Think()
-		local lvl = LocalPlayer():GetLevel()
-		local mon = LocalPlayer():GetMoney()
+		local lvl = me:GetLevel()
+		local mon = me:GetMoney()
 
 		if pl ~= lvl then
 			PopupLevel = CurTime()
@@ -211,13 +286,9 @@ function DarkHUD.Create()
 		pl, pm, pe = lvl, mon
 	end
 
-	local lvX = 0
 	local monY = 0
 
 	local helpa = 0
-
-	local mondiffY = 0
-
 
 	local monYMax = 36
 	local monYMin = -10
@@ -230,6 +301,7 @@ function DarkHUD.Create()
 	local lvbox = Color(50, 50, 50)
 
 	function f:PrePaint(w,h)
+
 		local ct = CurTime()
 
 		if ct - PopupLevel < 4 then
@@ -259,10 +331,10 @@ function DarkHUD.Create()
 
 			if monY > 2 then
 
-				local mtxt = Language.Currency .. BaseWars.NumberFormat(LocalPlayer():GetMoney())
+				local mtxt = Language.Currency .. BaseWars.NumberFormat(me:GetMoney())
 
 				surface.SetFont("OSB28")
-				local mw, mh = surface.GetTextSize(mtxt)
+				local mw, _ = surface.GetTextSize(mtxt)
 
 				draw.RoundedBox(6, 12, -monY - lvY, mw + 24 + 24, 32, boxcol)
 
@@ -322,7 +394,7 @@ function DarkHUD.Create()
 
 
 			if lvY > 2 then
-				local lv = tostring(LocalPlayer():GetLevel())
+				local lv = tostring(me:GetLevel())
 				surface.SetFont("OSB28")
 				local mw, mh = surface.GetTextSize(lv)
 
@@ -365,61 +437,58 @@ function DarkHUD.Create()
 
 	local facname
 
+	local function Mask(av, x, y, w2, h2)
+		draw.NoTexture()
+		surface.SetDrawColor(0, 0, 0, 255)
+		draw.DrawCircle(x+w2/2, y+h2/2, w2/2 + 2, 50)
+	end
+
+	local function Paint(av)
+		av:SetAlpha(255)
+		av:PaintManual()
+	end
+
 	function f:PostPaint(w, h)
 		local x, y = av:GetPos()
 		local w2, h2 = av:GetSize()
 
-		draw.SimpleText(LocalPlayer():Nick(), "OSB32", x + w2 + 13, y - 4, tcol, 0, 5)
+		local nameY = f.HeaderSize - fonts.NameHeight * 0.1
+		draw.SimpleText(me:Nick(), "DarkHUD_Name", x + w2 + 12, nameY, tcol, 0, 5)
 
 		surface.SetDrawColor(Color(255, 255, 255, 220))
-		surface.DrawMaterial("https://i.imgur.com/5BQxS4m.png", "faction.png", x + w2 + 24, y + 32, 24, 24)
-		local fac = LocalPlayer():GetFaction()
+		--surface.DrawOutlinedRect(x + w2 + 12, nameY, w, h)
+
+		local fac = me:GetFaction()
 
 		if lastfac ~= fac then
 			lastfac = fac
 			facname = faclen(lastfac)
 		end
+																												--  V i'm getting tired of source's retarded text-height-calculation
+		local _, fach = draw.SimpleText(facname, "DarkHUD_Faction", x + w2 + 12 + 20 + 8, nameY + fonts.NameHeight - 2, Color(255, 255, 255, 20), 0, 5)
 
-		draw.SimpleText(facname, "FactionFont", x + w2 + 24 + 20 + 8, y + 32 + 20, Color(255, 255, 255, 20), 0, 4)
+		surface.DrawMaterial("https://i.imgur.com/5BQxS4m.png", "faction.png", x + w2 + 12, nameY + fonts.NameHeight + 2, 24, 24)
 
-		local tm = LocalPlayer():Team()
+		local tm = me:Team()
 		local col = (tm~=0 and team.GetColor(tm)) or Color(100, 100, 100)
 		tcol = LC(tcol, col, 15)
 
-		render.SetStencilEnable(true)
+		draw.Masked(Mask, Paint, nil, nil, av, x, y, w2, h2)
 
-			render.ClearStencil()
-			render.SetStencilWriteMask( 1 )
-			render.SetStencilTestMask( 1 )
-
-			render.SetStencilCompareFunction( STENCIL_ALWAYS )
-			render.SetStencilPassOperation( STENCIL_REPLACE )
-
-			render.SetStencilReferenceValue( 1 ) --include
-
-			surface.SetDrawColor(Color(0, 0, 255, 255))
-			draw.NoTexture()
-			draw.DrawCircle(x+w2/2, y+h2/2, w2/2, 50)
-
-			render.SetStencilCompareFunction( STENCIL_ALWAYS )
-			render.SetStencilPassOperation( STENCIL_REPLACE )
-
-
-			render.SetStencilCompareFunction( STENCIL_EQUAL )
-			render.SetStencilFailOperation( STENCIL_KEEP )
-			render.SetStencilZFailOperation( STENCIL_KEEP )
-			render.SetStencilReferenceValue( 1 ) --include
-
-			av:SetAlpha(255)
-			av:PaintManual()
-
-		render.SetStencilEnable(false)
 
 		surface.SetDrawColor(tcol)
 		surface.DrawMaterial("https://i.imgur.com/VMZue2h.png", "circle_outline.png", x-3, y-3, w2+6, h2+6)
 	end
 
+	vls.HPFrac = 0
+	vls.ARFrac = 0
+
 	function vls:Paint(w, h)
+		local barH = math.floor(16 * Lerp(0.5, scale, 1) / 2) * 2 --brings to multiple of 2
+
+		if barH > 13 and barH < 21 then
+			barH = 16
+		end
 
 		local x, y = 12, av.Y
 		y = y - hs
@@ -427,107 +496,121 @@ function DarkHUD.Create()
 
 		--self:SetSize(f:GetWide(), f:GetTall() - hs)
 
-		hpfr = L(hpfr, (LocalPlayer():Health()/LocalPlayer():GetMaxHealth()), 15)
-		arfr = L(arfr, LocalPlayer():Armor()/100, 15)
+		local hpto = math.Clamp( me:Health() / me:GetMaxHealth(), 0, 1)
+		local arto = math.min(me:Armor() / 100, 1)
+
+		self:To("HPFrac", hpto, 0.4, 0, 0.2)
+		self:To("ARFrac", arto, 0.4, 0, 0.2)
+
+		local hpfr, arfr = self.HPFrac, self.ARFrac
 
 		local avx = x + 16 --contains X padding
-		local avy = y + 32 + 32 + 18 --contains Y padding
+		local avy = y + math.ceil(64*scale) + 8 --contains Y padding
 
 
-		local sx, sy = self:LocalToScreen(avx, avy)
 		local hpw = w - avx*2 - 48
-		--[[
-			Health & Armor
-		]]
-		local overheal = false
-		local overar = false
 
-		if hpfr > 1 then
-			overheal = true
-			hpfr = 1
+		local rndrad = barH/2
+
+		if rndrad > 8 and rndrad < 11 then  --16 is perfect because gmod corners are 8x8
+			rndrad = 8						--any more and they looked scuffed
 		end
 
-		if arfr > 1 then
-			overar = true
-			arfr = 1
-		end
+		local barsH = barH * 2 + 8
 
+		local barY = math.ceil(h / 2) - barsH / 2
+
+		--surface.DrawOutlinedRect(avx, barY, w - avx*2 - 48, barsH)
+		local sx, sy = self:LocalToScreen(avx, barY)
 
 		--[[
 			Health
 		]]
 
 			local hpw = w - avx*2 - 48
-			local round = (hpw*hpfr > 16 and math.Round(math.Clamp(hpw*hpfr - 8, 0, 8)))
+			local round = (hpw * hpfr > barH and math.Round(math.Clamp(hpw*hpfr - rndrad, 0, rndrad)))
 
-			draw.RoundedBox(8, avx, avy, w - avx*2 - 48, 16, Color(80, 80, 80))
+			draw.RoundedBox(rndrad, avx, barY, w - avx*2 - 48, barH, Color(80, 80, 80))
 
 			if not round then
 				--draw.RoundedBox(8, avx, avy, hpw, 16, Color(240, 70, 70))
-				if hpw*hpfr < 16 then
-					render.SetScissorRect(sx, sy, sx+hpw*hpfr, sy+16, true)
-						draw.RoundedBoxEx(8, avx, avy, 16, 16, Color(240, 70, 70), true, false, true, false)
+				if hpw*hpfr < barH then
+					render.SetScissorRect(sx, sy - 2, sx + (hpw * hpfr), sy + barH + 2, true)
+						draw.RoundedBoxEx(rndrad, avx, barY, barH, barH, Color(240, 70, 70), true, false, true, false)
 					render.SetScissorRect(0, 0, 0, 0, false)
 				else
 
-					draw.RoundedBox(8, avx, avy, hpw*hpfr, 16, Color(240, 70, 70))
+					draw.RoundedBox(rndrad, avx, barY, hpw*hpfr, barH, Color(240, 70, 70))
 
 				end
 
 			elseif round then
 
-				draw.RoundedBoxExEx(8, avx, avy, hpw*hpfr, 16, Color(240, 70, 70), 8, round, 8, round)
+				draw.RoundedBoxExEx(rndrad, avx, barY, hpw*hpfr, barH, Color(240, 70, 70), rndrad, round, rndrad, round)
 
 			end
 
-			draw.SimpleText(LocalPlayer():Health(), "OS28", w - avx*2 - 8, avy + 6, Color(255, 255, 255), 0, 1)
+			draw.SimpleText(LocalPlayer():Health(), "DarkHUD_VitalsNumber", avx + (w - avx * 2 - 48) + 4, barY + barH/2 - 1, Color(255, 255, 255), 0, 1)
 
 
 		--[[
 			Armor
 		]]
 
-			avy = avy + 24
-			local round = (hpw*arfr > 16 and math.Round(math.Clamp(hpw*arfr - 8, 0, 8)))
+			barY = barY + barH + 8
+			local round = (hpw*arfr > 16 and math.Round(math.Clamp(hpw*arfr - rndrad, 0, rndrad)))
 
-			draw.RoundedBox(8, avx, avy, w - avx*2 - 48, 16, Color(80, 80, 80))
+			draw.RoundedBox(rndrad, avx, barY, w - avx*2 - 48, barH, Color(80, 80, 80))
 
 			if not round then
 				if hpw*arfr < 16 then
-					render.SetScissorRect(sx, sy, sx+hpw*arfr, sy+16, true)
-						draw.RoundedBox(8, avx, avy, 16, 16, Color(40, 120, 255))
+					render.SetScissorRect(sx, sy, sx+hpw*arfr, sy + barH, true)
+						draw.RoundedBox(rndrad, avx, barY, 16, barH, Color(40, 120, 255))
 					render.SetScissorRect(0, 0, 0, 0, false)
 				else
 
-					draw.RoundedBox(8, avx, avy, hpw*arfr, 16, Color(40, 120, 255))
+					draw.RoundedBox(rndrad, avx, barY, hpw*arfr, barH, Color(40, 120, 255))
 
 				end
 
 			elseif round then
 
-				draw.RoundedBoxExEx(8, avx, avy, hpw*arfr, 16, Color(40, 120, 255), 8, round, 8, round)
+				draw.RoundedBoxExEx(rndrad, avx, barY, hpw*arfr, barH, Color(40, 120, 255), rndrad, round, rndrad, round)
 
 			end
 
-			draw.SimpleText(LocalPlayer():Armor(), "OS28", w - avx*2 - 8, avy + 6, Color(255, 255, 255), 0, 1)
+			draw.SimpleText(LocalPlayer():Armor(), "DarkHUD_VitalsNumber", avx + (w - avx * 2 - 48) + 4, barY + barH/2 - 1, Color(255, 255, 255), 0, 1)
 
 	end
 
 	function ecn:Paint(w,h)
+
 		local x, y = av:GetPos()
 
+		local barH = math.floor(16 * Lerp(0.5, scale, 1) / 2) * 2 --brings to multiple of 2
+		local rndrad = barH/2
+
+		local barsH = barH * 2 + 8
+		local barY = h/2 - barH/2
+
+		local iconSize = 24 * Lerp(0.5, scale, 1)
 		surface.SetDrawColor(255, 255, 255)
-		surface.DrawMaterial("https://i.imgur.com/8b0nZI7.png", "moneybag.png", x + 18, y + 48, 25, 24)
+		surface.DrawMaterial("https://i.imgur.com/8b0nZI7.png", "moneybag.png", x + 18, 4, iconSize, iconSize)
 
-		draw.SimpleText(Language.Currency .. BaseWars.NumberFormat(LocalPlayer():GetMoney()), "OS24", x + 50, y + 48 + 12, color_white, 0, 1)
-
-		surface.DrawMaterial("https://i.imgur.com/YYXglpb.png", "star.png", x + 18, y + 80, 24, 24)
-
-		draw.SimpleText(LocalPlayer():GetLevel(), "OS24", x + 50, y + 80 + 12, color_white, 0, 1)
+		draw.SimpleText(Language.Currency .. BaseWars.NumberFormat(LocalPlayer():GetMoney()), "DarkHUD_Money", x + 50, 4 + iconSize/2, color_white, 0, 1)
 
 
-		draw.RoundedBox(8, w - 204, y + 82, 128, 16, Color(75, 75, 75))
-		draw.RoundedBox(8, w - 204, y + 82, (128) * (LocalPlayer():GetXP() / LocalPlayer():GetXPNextLevel()), 16, Color(140, 80, 220))
+		local expBoxX = w - 180 * scale - 8
+		local expBoxY = barY
+
+		local expY = 4 + iconSize + 8*scale + iconSize/2
+		local expw, exph = draw.SimpleText(LocalPlayer():GetLevel(), "DarkHUD_Money", expBoxX + 64, 4 + iconSize/2, color_white, 1, 1)
+		expBoxY = 4 + iconSize/2 + exph/2 + 2
+
+		surface.DrawMaterial("https://i.imgur.com/YYXglpb.png", "star.png", expBoxX + 64 - expw/2 - 4 - iconSize, 4, iconSize, iconSize)
+
+		draw.RoundedBox(rndrad, expBoxX, expBoxY, 180*scale, barH, Color(75, 75, 75))
+		draw.RoundedBox(rndrad, expBoxX, expBoxY, 180*scale * (LocalPlayer():GetXP() / LocalPlayer():GetXPNextLevel()), barH, Color(140, 80, 220))
 	end
 
 end
