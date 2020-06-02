c6a8569472f16bb261d02ff5aff419f786aa0e90
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-06-02 22:53:50 +0700

    manually compute lighting on meme decals because garry

--- addons/memes/lua/autorun/client/mitty.lua
+++ addons/memes/lua/autorun/client/mitty.lua
@@ -11,7 +11,7 @@ local quad = {
 
 local novec, noang = Vector(), Angle()
 
-local function MakeMesh(dat)
+local function MakeMesh(dat, mat)
 	--[[
 	accepts a table:
 		{
@@ -24,12 +24,12 @@ local function MakeMesh(dat)
 
 	]]
 	if not dat.Mesh then
-
-		local m = Mesh()
+		print(mat)
+		local m = Mesh(mat)
 		dat.Mesh = m
 
 		local col = dat.Color or Color(200, 200, 200)
-		
+
 
 		mesh.Begin(m, MATERIAL_QUADS, 1)
 
@@ -88,7 +88,7 @@ local function RTRender(make2d, rt, func) --if make2d, make ur own 2d context
 
 	render.PushRenderTarget(rt)
 
-		if make2d then 
+		if make2d then
 			cam.Start2D()
 		end
 
@@ -98,20 +98,21 @@ local function RTRender(make2d, rt, func) --if make2d, make ur own 2d context
 			local ok, err = pcall(func, w, h)
 
 			render.OverrideAlphaWriteEnable(false, false)
-		
-		if make2d then 
+
+		if make2d then
 			cam.End2D()
 		end
 
 	render.PopRenderTarget()
 
-	if not ok then 
+	if not ok then
 		print("Decal error:", err)
 	end
 
 end
-	
+
 local function CreateRTAndMat(dec)
+
 	local rt = dec.RT or draw.GetRT(dec.Name .. "Decal", dec.RTW, dec.RTH)
 	dec.RT = rt
 
@@ -124,7 +125,7 @@ local function CreateRTAndMat(dec)
 
 	mat:SetTexture("$basetexture", rt:GetName()) --in the event it changed
 
-	if not dec.Mat or dec.RTUpdate or dec.NeedsMorePaint then 
+	if not dec.Mat or dec.RTUpdate or dec.NeedsMorePaint then
 		dec.NeedsMorePaint = (RTRender(true, rt, dec.RTPaint) ~= nil)
 	end
 
@@ -140,12 +141,12 @@ hook.Add("PostDrawTranslucentRenderables", "mittyvis", function()
 
 	local lp = EyePos()
 
-	for k,v in pairs(Memes.Decals) do 
-		local dist = lp:DistToSqr(v.PixPos or v.Pos)
+	for k,v in pairs(Memes.Decals) do
+		local dist = lp:DistToSqr(v.PixPos)
 
-		if dist > v.MaxDistSqr then 
+		if dist > v.MaxDistSqr then
 			v.Vis = 0
-		elseif dist < v.PixRadSqr then 
+		elseif dist < v.PixRadSqr then
 			v.Vis = 1
 		else
 			v.Vis = util.PixelVisible(v.PixPos or v.Pos, v.PixRad or 32, v.PixVisHandle)
@@ -157,101 +158,113 @@ end)
 
 Memes.Decals = Memes.Decals or {}
 
-hook.Add("PreDrawEffects", "mittypls", function(bd, bs)
-	if bd or bs then return end 
+hook.Remove("PostDrawViewModel", "mittypls")
+for k,v in pairs(hook.GetTable()) do if v.mittypls then hook.Remove(k, "mittypls") end end
 
+hook.Add("PostDrawTranslucentRenderables", "mittypls", function(bd, bs)
+
+	if bd or bs then return end
+
+	render.SuppressEngineLighting(true)
 	for k,v in pairs(Memes.Decals) do
-		if v.Vis > 0 then
+		if not v.Vis or v.Vis > 0 then
+
+			local lr = render.ComputeLighting(v.Middle, v.Normal)--:Unpack()
+			lr:Mul(v.LightMultiplication or 1)
 
 			local rt, mat = CreateRTAndMat(v)
-			local mesh = MakeMesh(v)
+			local mesh = MakeMesh(v, rt)
 
-			if not mesh then return end 
+			if not mesh then return end
 
 			local w, h = ScrW(), ScrH()
 
 			cam.PushModelMatrix(v.Matrix)
-			--cam.Start3D()
+
 				local ok, err = pcall(function()
 
+					v.Mat:SetVector("$color", lr)
 					render.SetMaterial(v.Mat)
 
 					mesh:Draw()
 
-					if LocalPlayer():FlashlightIsOn() then 
+					if LocalPlayer():FlashlightIsOn() then
+						v.Mat:SetVector("$color", Vector(1, 1, 1))
 						render.PushFlashlightMode(true)
 							mesh:Draw()
 						render.PopFlashlightMode()
 					end
 
 				end)
+
 			cam.PopModelMatrix()
 
 			--render.DrawLine(v.Pos, v.Pos + v.Normal * 32)
-			if not ok then 
+			if not ok then
 				print("failed during decal draw:", err)
-				return 
+				return
 			end
 		end
 	end
-
+	render.SuppressEngineLighting(false)
 end)
 
---[[
-	name: string
-	pos: vec
-	ang: angle
-	scale: vec
-	normal: vec for light calculation by mesh
-	col: color
 
-	rtpaint: function to paint on top of rt
-		return anything in this function if it needs to be rerendered next frame (for example, ur still downloading the required mat)
+function Memes.AddDecal(name, pos, ang, scale, normal, col, rtpaint, rtupd, pixpos, pixrad, mw, mh, rtw, rth, light)
 
-	rtupd: if true, RT will do rtpaint every frame 
+	--[[
+		name: string
+		pos: vec
+		ang: angle
+		scale: vec
+		normal: vec for light calculation by mesh
+		col: color
 
-	pixpos: where to check for pixel visibility, pos by default
-	pixrad: radius to check for pixel visibility, 32 by default
+		rtpaint: function to paint on top of rt
+			gets called with RT width and height args
+			return anything in this function if it needs to be rerendered next frame (for example, ur still downloading the required mat)
 
-	meshw = mesh width  ]
-	meshh = mesh height ] 64 by default
+		rtupd: if true, RT will do rtpaint every frame
 
-	rtw = RT width  ]
-	rth = RT height ] 128 by default
+		pixpos: where to check for pixel visibility, pos by default
+		pixrad: radius to check for pixel visibility, 32 by default
 
-	------------
-	
-	todo: make this function return a decal object so  
-		  you don't have to give so many args
-]]
+		meshw = mesh width  ]
+		meshh = mesh height ] 64 by default
 
-function Memes.AddDecal(name, pos, ang, scale, normal, col, rtpaint, rtupd, pixpos, pixrad, mw, mh, rtw, rth)
-	if not rtpaint then error("rtpaint mandatory") return end 
+		rtw = RT width  ]
+		rth = RT height ] 128 by default
+
+		------------
+		todo: make this function return a decal object so
+			  you don't have to give so many args
+	]]
+
+	if not rtpaint then error("rtpaint mandatory") return end
 
 	local d = {}
-	Memes.Decals[name] = d
 
-	d.Name = name 
+	d.Name = name
 
 	local mtrx = Matrix()
-	d.Matrix = mtrx 
+	d.Matrix = mtrx
+
+	scale = (isnumber(scale) and Vector(scale, scale, scale)) or scale
 
 	mtrx:Translate(pos)
-		--mtrx:SetAngles(ang)
 		mtrx:SetScale(scale)
 	mtrx:Translate(-pos)
-	
 
-	d.Normal = normal
+	d.Normal = normal or ang:Right()
 
-	d.Pos = pos 
-	d.Ang = ang 
+	d.Pos = pos
+	d.Ang = ang
 	d.Scale = scale
 
 	d.PixVisHandle = util.GetPixelVisibleHandle()
-	d.PixPos = pixpos 
-	d.PixRad = pixrad
-	d.PixRadSqr = (pixrad ^ 2) * 2
+	d.PixPos = pixpos or pos
+	d.PixRad = pixrad or 32
+	d.PixRadSqr = (d.PixRad ^ 2) * 2
 
 	d.RTPaint = rtpaint
 	d.RTUpdate = rtupd
@@ -263,6 +276,9 @@ function Memes.AddDecal(name, pos, ang, scale, normal, col, rtpaint, rtupd, pixp
 	d.RTH = rth or 128
 
 	d.MaxDistSqr = 1024^2
+	d.LightMultiplication = light
+	d.Middle = d.Pos + (d.Ang:Right() * d.MeshW * scale / 2) - (d.Ang:Up() * d.MeshH * scale / 2)
+	Memes.Decals[name] = d
 end
 
 
@@ -273,12 +289,12 @@ local mia, mitty
 
 hook.Add("ChatHUDEmotesUpdated", "mittymeme", function(col)
 	mia = col.MadeInAbyss
-	if not mia then return end 
+	if not mia then return end
 
 	mitty = mia:GetEmotes().MittyElevator
-	if not mitty then return end 
+	if not mitty then return end
 
-	if mitty then 
+	if mitty then
 		mitty:Download()
 
 		local where = Vector (-7823.2, -8613.3, -1293.35)
@@ -287,7 +303,7 @@ hook.Add("ChatHUDEmotesUpdated", "mittymeme", function(col)
 		local ang = Angle(-180, -90, 180)
 		local scale = Vector(0.3, 0.3, 0.3)
 
-		Memes.AddDecal("mitty", where, ang, scale, Vector(0, -1, 0), nil, 
+		Memes.AddDecal("mitty", where, ang, scale, Vector(0, -1, 0), nil,
 			function(w, h)
 				if not mitty:Exists() or (mitty:Exists() and mitty:IsDownloading()) then return true end --need downloading
 
@@ -306,12 +322,12 @@ local stuck_where = Vector (-820.6, 44.415222167969, 485)
 local stuck_ang = Angle(0, -111.6, 0)
 local scale = Vector(0.2, 0.2, 0.2)
 
-Memes.AddDecal("stuck", stuck_where, stuck_ang, scale, Vector(0.5, 1, 0), nil, 
+Memes.AddDecal("stuck", stuck_where, stuck_ang, scale, Vector(0.5, 1, 0), nil,
 	function(w, h)
 		draw.RoundedBox(16, 0, 0, w, h, ColorAlpha(Colors.DarkGray, 240))
 		draw.SimpleText("Getting stuck is PROHIBITED.", "MR72", w/2, 0, Colors.Red, 1, 0)
 		draw.SimpleText("If you are stuck, CEASE IMMEDIATELY.", "MR48", w/2, 72, Colors.Red, 1, 0)
-	end, 
+	end,
 false, stuck_where + stuck_ang:Right() * 128 * 0.2, 20, 256, 32, 1024, 128)
 
 local function addMaN(ffz)
@@ -324,18 +340,18 @@ local function addMaN(ffz)
 		local ang = Angle (180, 0, 180)
 		local scale = Vector(0.05, 0.05, 0.05)
 
-		Memes.AddDecal("pillarMaN", where, ang, scale, Vector(1, 0, 0), nil, 
+		Memes.AddDecal("pillarMaN", where, ang, scale, Vector(1, 0, 0), nil,
 			function(w, h)
 				if not man:Exists() or (man:Exists() and man:IsDownloading()) then return true end
 				surface.SetDrawColor(color_white)
 				man:Paint(0, 0, 128, 128)
-			end, 
-		false, where, 8)
+			end,
+		false, where, 8, nil, nil, nil, nil, 10)
 	end
 end
 
 hook.Add("ChatHUDFFZUpdated", "manmeme", addMaN)
 
-if Emotes and Emotes.Collections.FFZ then 
+if Emotes and Emotes.Collections.FFZ then
 	addMaN(Emotes.Collections.FFZ)
 end
