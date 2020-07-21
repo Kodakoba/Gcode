5fa83df7570477d76316b700dad2718bd513a6d3
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-07-21 21:08:33 +0700

    get rid of trailing spaces and garbage entities

--- addons/githubbed/lua/autorun/client/3d2dvgui.lua
+++ addons/githubbed/lua/autorun/client/3d2dvgui.lua
@@ -1,5 +1,5 @@
 --[[
-	
+
 3D2D VGUI Wrapper
 Copyright (c) 2015-2017 Alexander Overvoorde, Matt Stevens
 
@@ -61,13 +61,13 @@ end
 local function absolutePanelPos(pnl)
 	local x, y = pnl:GetPos()
 	local parents = getParents(pnl)
-	
+
 	for _, parent in ipairs(parents) do
 		local px, py = parent:GetPos()
 		x = x + px
 		y = y + py
 	end
-	
+
 	return x, y
 end
 
@@ -96,14 +96,14 @@ local function postPanelEvent(pnl, event, ...)
 	if not IsValid(pnl) or not pnl:IsVisible() or not pointInsidePanel(pnl, getCursorPos()) then return false end
 
 	local handled = false
-	
+
 	for i, child in pairs(table.Reverse(pnl:GetChildren())) do
 		if postPanelEvent(child, event, ...) then
 			handled = true
 			break
 		end
 	end
-	
+
 	if not handled and pnl[event] then
 		pnl[event](pnl, ...)
 		usedpanel[pnl] = {...}
@@ -151,8 +151,8 @@ end
 -- Mouse input
 
 hook.Add("KeyPress", "VGUI3D2DMousePress", function(_, key)
-	if not IsFirstTimePredicted() then return end 
-	
+	if not IsFirstTimePredicted() then return end
+
 	if key == IN_USE then
 		for pnl in pairs(inputWindows) do
 			if IsValid(pnl) then
@@ -162,7 +162,7 @@ hook.Add("KeyPress", "VGUI3D2DMousePress", function(_, key)
 				normal = pnl.Normal
 
 				local key = input.IsKeyDown(KEY_LSHIFT) and MOUSE_RIGHT or MOUSE_LEFT
-				
+
 				postPanelEvent(pnl, "OnMousePressed", key)
 			end
 		end
@@ -170,8 +170,8 @@ hook.Add("KeyPress", "VGUI3D2DMousePress", function(_, key)
 end)
 
 hook.Add("KeyRelease", "VGUI3D2DMouseRelease", function(_, key)
-	if not IsFirstTimePredicted() then return end 
-	
+	if not IsFirstTimePredicted() then return end
+
 	if key == IN_USE then
 		for pnl, key in pairs(usedpanel) do
 			if IsValid(pnl) then
@@ -196,7 +196,7 @@ function vgui.Start3D2D(pos, ang, res)
 	angle = ang
 	normal = ang:Up()
 	maxrange = 0
-	
+
 	cam.Start3D2D(pos, ang, res)
 end
 
@@ -216,7 +216,7 @@ end
 local Panel = FindMetaTable("Panel")
 function Panel:Paint3D2D()
 	if not self:IsValid() then return end
-	
+
 	-- Add it to the list of windows to receive input
 	inputWindows[self] = true
 
@@ -231,32 +231,32 @@ function Panel:Paint3D2D()
 	function gui.MouseY()
 		return (cy or 0) / scale
 	end
-	
+
 	-- Override think of DFrame's to correct the mouse pos by changing the active orientation
 	if self.Think then
 		if not self.OThink then
 			self.OThink = self.Think
-			
+
 			self.Think = function()
 				origin = self.Origin
 				scale = self.Scale
 				angle = self.Angle
 				normal = self.Normal
-				
+
 				self:OThink()
 			end
 		end
 	end
-	
+
 	-- Update the hover state of controls
 	local _, tab = checkHover(self)
-	
+
 	-- Store the orientation of the window to calculate the position outside the render loop
 	self.Origin = origin
 	self.Scale = scale
 	self.Angle = angle
 	self.Normal = normal
-	
+
 	-- Draw it manually
 	self:SetPaintedManually(false)
 		self:PaintManual()
