0a439f77f9497dc7d07a82fda0d088f4b161c470
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-07-30 14:29:01 +0700

    SIGNIFICANTLY speed up gm autorefresh

--- gamemodes/basewars/gamemode/include.lua
+++ gamemodes/basewars/gamemode/include.lua
@@ -1,60 +1,38 @@
-DeriveGamemode("sandbox")
 
-local function IncludeCS(File)
+AddCSLuaFile()
 
+BaseWars = BaseWars or {}
+
+BaseWars.LoadLog = Logger("BW-Load", Color(220, 100, 100))
+local rlm = Realm(true, true)
+BaseWars.LoadLog("Beginning inclusion %s", rlm)
+
+local t1 = SysTime()
+
+if not BaseWars.Loaded then
+	BaseWars.LoadLog("	Deriving...")
+	DeriveGamemode("sandbox")
+end
+
+local function includeCS(File)
 	include(File)
-
 	if SERVER then
-
 		AddCSLuaFile(File)
-
 	end
-
 end
 
-local function LoadFileCS(File)
 
-	if CLIENT then
+BaseWars.LoadLog("	Including shared...")
 
-		include(File)
-
-	else
-
-		AddCSLuaFile(File)
-
-	end
-
-end
-
-local function IncludeSV(file)
-
-	if SERVER then
-
-		include(file)
-
-	end
-
-end
-
-do
-
-	IncludeCS("shared.lua")
-	IncludeCS("language.lua")
-	IncludeCS("config.lua")
-
-	IncludeCS("shared/spawnmenu.lua")
-	IncludeCS("shared/sh_perks.lua")
-
-	if BaseWars.Config.ExtraStuff then
-		IncludeCS("shared/playuhr.lua")
-		IncludeCS("shared/customnick.lua")
-	end
+	includeCS("shared.lua")
+	includeCS("language.lua")
+	includeCS("config.lua")
 
 	if ulib or ulx then
-		IncludeCS("integration/bw_admin_ulx.lua")
+		includeCS("integration/bw_admin_ulx.lua")
 	end
 
-end
+
 
 local path = "basewars/gamemode/"
 
@@ -62,36 +40,21 @@ local function shouldInclude(fn)
 	if fn:match("_ext") then return false, false end
 end
 
-FInc.Recursive(path .. "shared/*", _SH, nil, shouldInclude)
-FInc.Recursive(path .. "client/*", _CL, nil, shouldInclude)
-FInc.Recursive(path .. "server/*", _SV, nil, shouldInclude)
 
-include("modules.lua")
-AddCSLuaFile("modules.lua")
+BaseWars.LoadLog("	Recursive including realm folders...")
 
---[[do
+	FInc.Recursive(path .. "shared/*", _SH, nil, shouldInclude)
+	FInc.Recursive(path .. "client/*", _CL, nil, shouldInclude)
+	FInc.Recursive(path .. "server/*", _SV, nil, shouldInclude)
 
-	LoadFileCS("client/cl_items.lua")
-	LoadFileCS("client/cl_bwmenu.lua")
+BaseWars.LoadLog("	Loading module includer")
 
-	LoadFileCS("client/cl_status.lua")
+	include("modules.lua")
+	AddCSLuaFile("modules.lua")
 
-	LoadFileCS("client/cl_font_disaster.lua")
-	LoadFileCS("client/cl_perks.lua")
-	LoadFileCS("client/cl_suggestions.lua")
+local t2 = SysTime()
 
-	LoadFileCS("client/cl_playmus.lua")
+BaseWars.LoadLog("Included %s all in %.3fs!", rlm, t2 - t1)
 
-	for k,v in pairs(file.Find("basewars/gamemode/client/*.lua", "LUA")) do 
-		LoadFileCS("basewars/gamemode/client/" .. v)
-	end
-end
 
-do
-    IncludeSV("server/antifaggot.lua")
-	IncludeSV("server/commands.lua")
-	IncludeSV("server/hooks.lua")
-
-	IncludeSV("server/printers.lua")
-	IncludeSV("server/morelang.lua")
-end]]
+BaseWars.Loaded = true
