0e643048a8da83e06b5483231820a9dbc81ff2ff
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-07-30 14:15:59 +0700

    LibItUp inclusion text is :ok_hand: :ok_hand: :ok_hand:

--- addons/just_lib_it_up/lua/autorun/!!libitup.lua
+++ addons/just_lib_it_up/lua/autorun/!!libitup.lua
@@ -97,6 +97,8 @@ function IncludeFolder(name, realm, nofold)	--This function will be used both by
 
 end
 
+local t1 = SysTime()
+
 include(path .. "classes.lua") -- base class goes first
 
 IncludeFolder(path .. "extensions/*", _SH) -- then extensions
@@ -104,9 +106,6 @@ IncludeFolder(path .. "classes/*", _SH)
 
 loading = false
 
-local str = "\n/------------\n Lib-it-up loaded!\n %d files included. \n-------------/\n\n"
-
-MsgC(Color(230, 230, 110), str:format(files))
 
 hook.Run("LibbedItUp")
 hook.Run("LibItUp")
@@ -125,3 +124,40 @@ FInc.Recursive("lib_deps/server/*", _SV)
 hook.Add("InitPostEntity", "InittedGlobal", function()
 	EntityInitted = true
 end)
+
+local t2 = SysTime()
+
+
+-- centering the fancy loaded text
+
+local l1 = "LibItUp loaded!"
+local l2 = "%d files included."
+local l3 = "Took %.3fs."
+
+l2 = l2:format(files)
+l3 = l3:format(t2 - t1)
+
+local longest_line = math.ceil(math.max(#l1, #l2, #l3) / 2) * 2 + 1 -- bring to closest odd number
+
+local function calcWidth(tx)
+	local amt1 = math.floor( (longest_line - #tx) / 2)
+	local amt2 = math.ceil( (longest_line - #tx) / 2)
+	local spaces1 = (" "):rep(amt1)
+	local spaces2 = (" "):rep(amt2)
+
+	return "\n|" 	.. spaces2 .. tx .. spaces1 .. "|"
+end
+
+
+local str = calcWidth(l1)
+str = str .. calcWidth(l2)
+str = str .. calcWidth(l3)
+
+local top = "□" .. ("―"):rep(longest_line) .. "□"
+local bottom = "□" .. ("―"):rep(longest_line) .. "□"
+
+MsgC(Color(230, 230, 110), "\n",
+
+	top,
+		str, 	"\n",
+	bottom, 		"\n\n")
