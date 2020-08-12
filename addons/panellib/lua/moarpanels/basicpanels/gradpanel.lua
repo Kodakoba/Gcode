2bead96ed9ec6ff8d4c9901ca9852f3c4574f43f
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-08-12 23:18:35 +0700

    create gradpanel

---
+++ addons/panellib/lua/moarpanels/basicpanels/gradpanel.lua
@@ -0,0 +1,31 @@
+
+local PANEL = {}
+
+function PANEL:Init()
+	self.GradColor = Color(10, 10, 10, 250)
+	self.Color = Color(200, 200, 200)
+	self.GradSize = 4
+end
+
+function PANEL:SetColor(col, g, b, a)
+	if IsColor(col) then
+		self.Color = col
+		return
+	end
+
+	local c = self.Color
+	c.r = col or 70
+	c.g = g or 70
+	c.b = b or 70
+	c.a = a or 255
+end
+
+function PANEL:Paint(w, h)
+	surface.SetDrawColor(self.Color:Unpack())
+	surface.DrawRect(0, 0, w, h)
+
+	surface.SetDrawColor(self.GradColor:Unpack())
+	self:DrawGradientBorder(w, h, self.GradSize, self.GradSize)
+end
+
+vgui.Register("GradPanel", PANEL, "Panel")
