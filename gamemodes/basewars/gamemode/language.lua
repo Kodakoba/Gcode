429ff876ce6d3653159e7234dee2035ab85cb947
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-08-25 16:59:46 +0700

    Update language.lua

--- gamemodes/basewars/gamemode/language.lua
+++ gamemodes/basewars/gamemode/language.lua
@@ -4,20 +4,22 @@ local CURRENCY = "£"
 
 Language = Language or {}
 
-Language.Currency = "£"
-Language.CURRENCY = Language.Currency 
+Language.Currency = CURRENCY
+Language.CURRENCY = CURRENCY
+Language.Price = CURRENCY .. "%s"
 
 Language.eval = function(self, key, ...)
 	if Language[key] then 
-		return Language[key]:format(...)
+		return Language[key]:format(...), true
 	end
-	return ("[Invalid language: %s]"):format(key)
+	return ("[Invalid language: %s]"):format(key), false
 end
 
 Language.__index = function(self, key)
 	return ("[Invalid language: %s]"):format(key)
 end
 Language.__call = Language.eval 
+
 
 
 Language.NoPower 		= "No power!"
@@ -42,6 +44,10 @@ Language.SpawnMenuBuyConfirm = "Are you sure you want to purchase %s for " .. La
 
 Language.Yes = "Yes"
 Language.No = "No"
+
+Language.Tip = "Tip!"
+Language.PrinterUpgradeTip = "Type /upg or /upgrade while looking at a printer to upgrade it.\nYou can specify how many levels you want to upgrade something by.\nTry it now!"
+Language.PrinterUpgradeTipFont = "OS28"
 
 setmetatable(Language, Language)
 --[[
