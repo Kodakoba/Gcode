38a95b69c9a244a470df3bca1c1252908a517347
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-08-29 17:26:58 +0700

    hook.Once(Ret) can gen new id's for valid objects

--- addons/just_lib_it_up/lua/lib_it_up/extensions/hook.lua
+++ addons/just_lib_it_up/lua/lib_it_up/extensions/hook.lua
@@ -2,15 +2,27 @@ local ID = 0
 
 function hook.Once(hookname, hookid, cb)
 
+	local args = {}
+
 	if isfunction(hookid) then
 		cb = hookid
 		hookid = "hookOnce" .. ID
 		ID = ID + 1
 	end
 
+	local canIndex = isentity(hookid) or ispanel(hookid) or istable(hookid)
+
+	if canIndex and hookid.IsValid then
+		args[1] = hookid
+
+		hookid = ("hookOnce:%p"):format(hookid) .. ":" .. ID
+		ID = ID + 1
+	end
+
 	hook.Add(hookname, hookid, function(...)
+		table.InsertVararg(args, ...)
 		hook.Remove(hookname, hookid)
-		cb(...)
+		cb(unpack(args))
 	end)
 
 end
@@ -18,14 +30,27 @@ end
 
 function hook.OnceRet(hookname, hookid, cb)
 
+	local args = {}
+
 	if isfunction(hookid) then
 		cb = hookid
 		hookid = "hookOnceRet" .. ID
 		ID = ID + 1
 	end
 
+	local canIndex = isentity(hookid) or ispanel(hookid) or istable(hookid)
+
+	if canIndex and hookid.IsValid then
+		args[1] = hookid
+
+		hookid = ("hookOnce:%p"):format(hookid) .. ":" .. ID
+		ID = ID + 1
+	end
+
 	hook.Add(hookname, hookid, function(...)
-		local ok, ret = pcall(cb, ...)
+		table.InsertVararg(args, ...)
+
+		local ok, ret = pcall(cb, unpack(args))
 		if ret ~= false then
 			hook.Remove(hookname, hookid)
 			if not ok then
