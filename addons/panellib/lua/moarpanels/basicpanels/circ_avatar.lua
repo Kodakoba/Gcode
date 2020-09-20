9249b6737e1d8f13f698e0778948f34659f9a2f5
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-09-20 19:54:46 +0700

    circular avatars

---
+++ addons/panellib/lua/moarpanels/basicpanels/circ_avatar.lua
@@ -0,0 +1,42 @@
+local AV = {}
+
+local err = Material("__error")
+
+function AV:Init()
+	self.Rounding = 16
+end
+
+function AV:StencilAvatar(w, h)
+	surface.SetDrawColor(255, 255, 255)
+	--surface.DrawRect(0, 0, w, h)
+	draw.NoTexture()
+	draw.RoundedStencilBox(self.Rounding, 0, 0, w, h, color_white)
+end
+
+
+function AV:Paint(w, h)
+	
+	--draw.BeginMask(self.StencilAvatar, self, w, h)
+	--draw.DrawOp()
+	
+	--[[local x, y = self:LocalToScreen(0, 0)
+	self._w, self._h = self:GetSize()
+	self:SetSize(512, 512)
+
+	draw.EnableMaskCircle(0, 0, 512, 512)]]
+	self:Emit("Paint", w, h)
+	draw.BeginMask(self.StencilAvatar, self, w, h)
+	draw.DrawOp()
+end
+
+function AV:PaintOver(w, h)
+
+	--draw.DisableMaskCircle(0, 0, h)
+	--self:SetSize(self._w, self._h)
+	self:Emit("PreDemaskPaint", w, h)
+	draw.FinishMask()
+	self:Emit("PaintOver", w, h)
+end
+
+vgui.Register("CircularAvatar", AV, "AvatarImage")
+
