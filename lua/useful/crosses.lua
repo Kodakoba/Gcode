fc888f235012920bf3985f0063b0289b6b5a0200
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-11-21 14:51:57 +0700

    kewl debugoverlay serverside nw

--- lua/useful/crosses.lua
+++ lua/useful/crosses.lua
@@ -1,23 +1,55 @@
-if SERVER then
-    util.AddNetworkString("cross")
+debugoverlay = debugoverlay or {}
 
-    function cross(vec, sz, lf, col, igz)
-        net.Start("cross")
-            net.WriteVector(vec)
-            net.WriteUInt(sz, 8)
-            net.WriteFloat(lf or 1)
-            net.WriteColor(col or color_white)
-            net.WriteBool(igz)
-        net.Broadcast()
-    end
+local funcs = {
+	{"Axis", "Vector", "Angle", "UInt", "Float", "Bool"},
+	{"Box", "Vector", "Vector", "Vector", "Float", "Color"},
+	{"BoxAngles", "Vector", "Vector", "Vector", "Angle", "Float", "Color"},
+	{"Cross", "Vector", "UInt", "Float", "Color", "Bool"},
+	{"EntityTextAtPosition", "Vector", "UInt", "String", "Float", "Color"},
+	{"Grid", "Vector"},
+	{"Line", "Vector", "Vector", "Float", "Color", "Bool"},
+	{"ScreenText", "Float", "Float", "String", "Float", "Color"},   -- probably useless serverside
+	{"Sphere", "Vector", "UInt", "Float", "Color", "Bool"},
+	{"SweptBox", "Vector", "Vector", "Vector", "Vector", "Angle", "Float", "Color"},
+	{"Text", "Vector", "String", "Float", "Bool"},
+	{"Triangle", "Vector", "Vector", "Vector", "Float", "Color"}
+}
+
+
+if SERVER then
+	util.AddNetworkString("dbg")
+
+	local function sendDbg(id, ...)
+		net.Start("dbg")
+			net.WriteUInt(id, 4)
+			net.WriteUInt(select("#", ...), 4)
+			for i=2, #funcs[id] do
+				local nm = funcs[id][i]
+				if select(i-1, ...) ~= nil then
+					net["Write" .. nm] ( (select(i-1, ...)), 16 )
+				end
+			end
+		net.Broadcast()
+	end
+
+	for k,v in ipairs(funcs) do
+		debugoverlay[ v[1] ] = Curry(sendDbg, k)
+	end
 
 else
 
-    net.Receive("cross", function()
-        local where, sz, lf, col, igz = net.ReadVector(), net.ReadUInt(8), net.ReadFloat(), net.ReadColor(), net.ReadBool()
-        debugoverlay.Cross(where, sz, lf, col, igz)
-    end)
+	net.Receive("dbg", function()
+		local what = net.ReadUInt(4)
+		local argsAmt = net.ReadUInt(4)
+		local fdat = funcs[what]
+		local args = {}
 
-    cross = debugoverlay.Cross
+		for i=2, math.min(#fdat, argsAmt + 1) do
+			args[i - 1] = net["Read" .. fdat[i]] (16)
+		end
+
+		--local where, sz, lf, col, igz = net.ReadVector(), net.ReadUInt(8), net.ReadFloat(), net.ReadColor(), net.ReadBool()
+		debugoverlay[fdat[1]](unpack(args))
+	end)
 
 end
