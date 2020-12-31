04b86f0041f333cbecf67a7a9ca961f1d50e96f1
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-12-31 19:23:46 +0700

    misc util functions

--- addons/just_lib_it_up/lua/lib_it_up/extensions/bit.lua
+++ addons/just_lib_it_up/lua/lib_it_up/extensions/bit.lua
@@ -1,3 +1,7 @@
+function tobit(what)
+	return what and 1 or 0
+end
+
 function bit.GetLast(num, n)
 	return num % (2^n)
 end
@@ -21,4 +25,12 @@ local select = select
 function bit.Has(num, ...)
 	local bor = bit.bor(...)
 	return bit.band(num, bor) == bor
+end
+
+-- returns a number with `num` LS bits as 1's
+-- because i get autism every time i try to remember the algo lol
+
+-- eg: bit.Fill(8) -> 0xFF
+function bit.Fill(num)
+	return (bit.lshift(1, num) - 1)
 end
--- addons/just_lib_it_up/lua/lib_it_up/libraries/uniq.lua
+++ addons/just_lib_it_up/lua/lib_it_up/libraries/uniq.lua
@@ -16,9 +16,14 @@ end
 uniq.__seqs = uniq.__seqs or {} -- haha seks
 local seqs = uniq.__seqs
 
-function uniq.Seq(id)
+function uniq.Seq(id, sz)
 	id = id or ">tfw no id"
 	local ret = (seqs[id] and seqs[id] + 1) or 1
+
+	if isnumber(sz) then
+		ret = bit.band(ret, bit.Fill(sz))
+	end
+
 	seqs[id] = ret
 	return ret
 end
