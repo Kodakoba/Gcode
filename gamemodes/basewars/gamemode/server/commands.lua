736c5f6fbda4a1b6a59c93b5fd72fca03dcce423
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-12-31 19:38:03 +0700

    spaces and move to new ent tracker

--- gamemodes/basewars/gamemode/server/commands.lua
+++ gamemodes/basewars/gamemode/server/commands.lua
@@ -193,8 +193,8 @@ local function Upgrade(ply, amt, ent)
 
     local amnt=tonumber(amount) or 1
     amnt = math.min(amnt, Ent.MaxLevel or 1)
-   
-    for i=1, amnt do 
+
+    for i=1, amnt do
 		local ok = Ent:Upgrade(ply)
 		if ok==false then break end
 	end
@@ -208,7 +208,7 @@ util.AddNetworkString("BW.Upgrade")
 net.Receive("BW.Upgrade", function(len, ply)
 
 	local ent = net.ReadEntity()
-	if not Upgradable(ply, ent) then return end 
+	if not Upgradable(ply, ent) then return end
 	Upgrade(ply, 1, ent) --no support for multiple amounts
 end)
 BaseWars.Commands.AddCommand({"max"}, function(ply)
@@ -217,7 +217,7 @@ BaseWars.Commands.AddCommand({"max"}, function(ply)
 
 	local Ent = trace.Entity
 	if not Upgradable(ply, Ent) then return false end
-    
+
     for i=1, Ent.MaxLevel - Ent.Level do
 
 		local res = Ent:Upgrade(ply)
@@ -276,13 +276,10 @@ end, false)
 BaseWars.Commands.AddCommand({"sellall"}, function(ply, line, who, amount)
 
 	timer.Simple(15, function()
-		
-		if not IsPlayer(ply) then return end
-		if not BWOwners[ply] then ply:ChatAddText("Something went... wrong?") return end 
+		if not IsPlayer(ply) then return end -- left?
 		if ply:InRaid() then ply:ChatAddText(Color(50, 150, 250), "Your /sellall command was cancelled because you were raided.") return end
-		 
 
-		for k,v in ValidPairs(BWOwners[ply]) do 
+		for k,v in ValidPairs(BaseWars.Ents.GetOwnedBy(ply)) do
 			if not v.CurrentValue then continue end
 
 			BaseWars.UTIL.PayOut(v, ply)
@@ -307,8 +304,8 @@ BaseWars.Commands.AddCommand({"dw", "dropwep"}, function(ply)
 		if BaseWars.Config.WeaponDropBlacklist[Class] then return false end
 		local bkup = {}
 
-		for k,v in pairs(BackupWeaponKeys) do 
-			if Wep[v] then 
+		for k,v in pairs(BackupWeaponKeys) do
+			if Wep[v] then
 				bkup[v] = (istable(Wep[v]) and table.Copy(Wep[v])) or Wep[v]
 			end
 		end
@@ -367,9 +364,9 @@ BaseWars.Commands.AddCommand({"bounty", "place", "placebounty"}, function(ply, l
 
 	amt = amount:lower():Trim()
 	if amt:match("nan") then return false, "Can't break the system mate" end
-	
+
 	amount = tonumber(amount) or 0
-	
+
 	if amount <= 0 then return false, false, "Can't break the system mate" end
 
 	local result, error = Targ:PlaceBounty( ply, amount )
@@ -385,7 +382,7 @@ local npc
 
 BaseWars.Commands.AddCommand({"help"}, function(ply, line, who, amount)
 
-	if not IsValid(npc) then 
+	if not IsValid(npc) then
 		for k,v in pairs(ents.FindByClass("bw_npc")) do
 			npc = v
 		end
