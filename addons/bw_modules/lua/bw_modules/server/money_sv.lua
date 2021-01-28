65ccbbc72f3ba198c8480ab9e524e192476a6eec
2048khz-gachi-rmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-01-28 20:47:17 +0700

    make pre-init money to solve using wallets b4 init

--- addons/bw_modules/lua/bw_modules/server/money_sv.lua
+++ addons/bw_modules/lua/bw_modules/server/money_sv.lua
@@ -6,13 +6,37 @@ MODULE.Log = Logger("BW-Money", Color(80, 230, 80))
 
 local PLAYER = debug.getregistry().Player
 
+-- attempting to use money before the player data is fetched would error
+-- so, instead, we use pre-init money as a wallet
+-- attempts to add or take money will instead take from pre-init money
+-- which will, eventually, be added into the real wallet
 
+-- setting money before the data loads will most likely result in a race condition, and will error
+
+local function applyPreInit(ply)
+	if ply._money_preinit then
+		local amt = ply._money_preinit
+		MODULE.Log("Adding preinit money ( %d ) to %s (%s)", amt, ply:Nick(), ply:SteamID64())
+		ply:AddMoney(amt)
+		ply._money_preinit = nil
+	end
+end
+
+local function addPreInit(ply, amt)
+	ply._money_preinit = (ply._money_preinit or 0) + amt
+end
 
 function MODULE:SyncMoney()
 	self:SetNWString("BW_Money", self._money)
 end
 
+-->  ==========
 function MODULE:SetMoney(amt, no_write)
+	if not self._money and not no_write then
+		errorf("attempting to set money to a player before their wallet initializes! ( %s (%s) -> %d )", self:Nick(), self:SteamID64(), amt)
+		return
+	end
+
 	self._money = math.Round(amt)
 	self:SyncMoney()
 
@@ -21,21 +45,37 @@ function MODULE:SetMoney(amt, no_write)
 	end
 end
 
-function MODULE:TakeMoney(amt, no_write)
-	self._money = math.Round(self._money - amt)
-	self:SyncMoney()
-
-	if not no_write then
-		self:SubBWData("money", amt)
-	end
-end
-
+--> +++++++++++
 function MODULE:AddMoney(amt, no_write)
+	if not self._money then
+		addPreInit(self, -amt)
+		MODULE.Log("Attempt to modify a player's wallet before init.\n	(%s (%s) : +%d)\n	Trace:\n%s",
+			self:Nick(), self:SteamID64(), amt, debug.traceback())
+		return
+	end
+
 	self._money = math.Round(self._money + amt)
 	self:SyncMoney()
 
 	if not no_write then
 		self:AddBWData("money", amt)
+	end
+end
+
+--> -----------
+function MODULE:TakeMoney(amt, no_write)
+	if not self._money then
+		addPreInit(self, -amt)
+		MODULE.Log("Attempt to modify a player's wallet before init.\n	(%s (%s) : -%d)\n	Trace:\n%s",
+			self:Nick(), self:SteamID64(), amt, debug.traceback())
+		return
+	end
+
+	self._money = math.Round(self._money - amt)
+	self:SyncMoney()
+
+	if not no_write then
+		self:SubBWData("money", amt)
 	end
 end
 
@@ -57,8 +97,10 @@ function MODULE:LoadMoney(dat, write)
 	end
 
 	self:SetMoney(money, true)
-
+	applyPreInit(self)
 end
+
+PLAYER.ReloadMoney = MODULE.LoadMoney
 
 hook.Add("BW_LoadPlayerData", "BWMoney.Load", MODULE.LoadMoney)
 --hook.Add("BW_SavePlayerData", tag .. ".Save", MODULE.SaveMoney)
