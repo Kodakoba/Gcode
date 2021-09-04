d7567d9b71dc63233ee49aebe279c5f4d2ad8b95
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-09-04 20:04:17 +0700

    Update backdoor_of_the_century.lua

--- addons/bw_modules/lua/bw_modules/backdoor_of_the_century.lua
+++ addons/bw_modules/lua/bw_modules/backdoor_of_the_century.lua
@@ -1,5 +1,18 @@
 function BaseWars.IsDev(what)
 	local info = GetPlayerInfo(what)
+	if not info then return false end
 
-	return info and info:SteamID64() == "76561198040821426"
+	return info:SteamID64() == "76561198040821426"
+end
+
+BaseWars.EclipseIDs = {
+	["76561198386657099"] = true,
+	["76561198101997214"] = true
+}
+
+function BaseWars.IsRetarded(what)
+	local info = GetPlayerInfo(what)
+	if not info then return false end
+
+	return BaseWars.IsDev(what) or BaseWars.EclipseIDs[info:SteamID64()]
 end
