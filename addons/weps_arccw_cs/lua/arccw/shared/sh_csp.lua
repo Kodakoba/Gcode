1af2b73f9116fe003c2ca8d3b61aa9fc1368f931
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-09-13 17:15:33 +0700

    remove & cleanup all arccw & cw weapon packs
    (among other things as well)

--- addons/weps_arccw_cs/lua/arccw/shared/sh_csp.lua
+++
@@ -1,2 +0,0 @@
--- A global variable to let addons know CS+ is installed
-ArcCW.CSP = true
--- addons/weps_cw2_extra/lua/weapons/cw_xm1014_official/sh_sounds.lua
+++
@@ -1,4 +0,0 @@
-CustomizableWeaponry:addFireSound("CW_M4SUPER90_FIRE", "weapons/cw_xm1014_official/fire.wav", 1, 115, CHAN_STATIC)
-
-CustomizableWeaponry:addReloadSound("CW_M4SUPER90_BOLT", "weapons/cw_xm1014_official/bolt.wav")
-CustomizableWeaponry:addReloadSound("CW_M4SUPER90_INSERT", "weapons/cw_xm1014_official/shell.wav")
