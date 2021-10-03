6c412203c5d46f0af366416ff22f7d007fea4250
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-10-03 12:49:12 +0700

    add fading door tool + gslt kekw

--- addons/core_bw_modules/lua/bw_modules/server/convars_sv.lua
+++ addons/core_bw_modules/lua/bw_modules/server/convars_sv.lua
@@ -1,7 +1,19 @@
+GSLT_Set = false
+
 function fuckyou()
 	if jit.os == "Linux" then
 		RunConsoleCommand("hostname", "lodestar | dev alpha")
+		if not GSLT_Set then
+		    RunConsoleCommand("sv_setsteamaccount", "6F077E7730DE463EDF6E19BC4D343E4C")
+		    GSLT_Set = true
+		end
+	else
+		if not GSLT_Set then
+		    RunConsoleCommand("sv_setsteamaccount", "74E5FAD9CD89DC9EB6072C344D75185E")
+		    GSLT_Set = true
+		end
 	end
+
 	--RunConsoleCommand("sv_downloadurl", "https://vaati.net/Gachi/garrysmod")
 	RunConsoleCommand("sv_downloadurl", "http://fr-gra-devserver-source01.crident.net/25017be5/")
 	RunConsoleCommand("sv_loadingurl", "https://vaati.net/Gachi/loading.html")
