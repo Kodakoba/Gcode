91ebe526108b5aa2fc9f22ed52ad0b8179dd6755
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-10-04 17:15:08 +0700

    add .restart and silence .map

--- addons/meta_cum/lua/cum/commands/map.lua
+++ addons/meta_cum/lua/cum/commands/map.lua
@@ -42,6 +42,23 @@ end)
 	:AddStringArg(false, "Map to switch to.")
 	:AddNumberArg(true, 10, "Countdown timer.")
 
+	:SetSilent(true)
+
 	:SetReportFunc(function(self, rply, caller, map, time)
 		return "{1} is switching map to {2} in {3} seconds.", {[2] = "<col=240,210,100>" .. found, [3] = "<col=100,220,100>" .. tostring(time)}
+	end)
+
+CUM.AddCommand({"reboot", "restart"}, function(ply, time)
+	local time = math.max(tonumber(time) or 20, 1)
+
+	aowl.CountDown(time, "RESTARTING SERVER" .. (reason and reason ~= "" and Format(" (%s)", reason) or ""), function()
+		game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
+	end)
+end)
+	:AddNumberArg(true, 20, "Countdown timer.")
+
+	:SetSilent(true)
+
+	:SetReportFunc(function(self, rply, caller, time)
+		return "{1} is restarting server in {2} seconds!", {[3] = "<col=100,220,100>" .. tostring(time)}
 	end)
