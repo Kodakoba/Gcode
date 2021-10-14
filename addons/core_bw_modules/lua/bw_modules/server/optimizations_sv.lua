2ba892eae1f3aeb9a61b6926611d1aade17f5235
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-10-14 11:44:45 +0700

    optimizations_sv (currently just physics settings)

---
+++ addons/core_bw_modules/lua/bw_modules/server/optimizations_sv.lua
@@ -0,0 +1,10 @@
+physenv.SetPerformanceSettings({
+    LookAheadTimeObjectsVsObject      =     0.15,
+    LookAheadTimeObjectsVsWorld       =     0.3,
+    MaxAngularVelocity                =  7272.7275390625,
+    MaxCollisionChecksPerTimestep     = 10000,
+    MaxCollisionsPerObjectPerTimestep =    3,
+    MaxFrictionMass                   =  2500,
+    MaxVelocity                       =  4000,
+    MinFrictionMass                   =    10
+})
