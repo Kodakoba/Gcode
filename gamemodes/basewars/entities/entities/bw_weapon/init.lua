82f7948e2abbcd40a6c6c0d1ab0c35979cea7487
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-10-19 23:21:37 +0700

    give ammo when buying a gun

--- gamemodes/basewars/entities/entities/bw_weapon/init.lua
+++ gamemodes/basewars/entities/entities/bw_weapon/init.lua
@@ -59,33 +59,38 @@ function ENT:Use(act, call, usetype, value)
 	local ply = act
 
 	if Class == "csgo_default_knife" then
-		if IsGroup(ply, "vip") then
 
+		if IsGroup(ply, "vip") then
 			local ktype = "default"
 
 			if ply.KnifeType then
-
 				for k,v in pairs(kt) do
-					if v==ply.KnifeType then ktype = v break end
+					if v == ply.KnifeType then ktype = v break end
 				end
-				ply:Give("csgo_"..ktype)
+				ply:Give("csgo_" .. ktype)
 				self:Remove()
 				return
 			end
-
-
 		end
+
 	end
 
 	if IsValid(Wep) then
 		local Clip = Wep.Primary and Wep.Primary.DefaultClip
-
-		ply:GiveAmmo(Clip or 30, Wep:GetPrimaryAmmoType())
+		ply:GiveAmmo((Clip and Clip * 2) or 30, Wep:GetPrimaryAmmoType())
 	else
 		Wep = ply:Give(Class)
+		if not IsValid(Wep) then
+			errNHf("Failed to give weapon class `%s` - invalid.", Wep)
+			return
+		end
+
 		if self.Backup then
 			table.Merge(Wep:GetTable(), self.Backup)
 		end
+
+		local Clip = Wep.Primary and Wep.Primary.DefaultClip
+		ply:GiveAmmo((Clip and Clip * 3) or 30, Wep:GetPrimaryAmmoType())
 
 		hook.Run("BW_WeaponPickedUp", self, Wep, ply)
 	end
