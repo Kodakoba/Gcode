617c04d5de70a7da3fb696118e0962a17acc7a4e
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-11-07 21:26:27 +0700

    loading URL as a setting

--- addons/core_bw_modules/lua/bw_modules/server/convars_sv.lua
+++ addons/core_bw_modules/lua/bw_modules/server/convars_sv.lua
@@ -27,7 +27,8 @@ function fuckyou()
 
 	--RunConsoleCommand("sv_downloadurl", "https://vaati.net/Gachi/garrysmod")
 	RunConsoleCommand("sv_downloadurl", "http://fr-gra-devserver-source01.crident.net/25017be5/")
-	RunConsoleCommand("sv_loadingurl", "https://vaati.net/Gachi/loading.html")
+	RunConsoleCommand("sv_loadingurl", Settings.Get("LoadingURL",
+		"https://vaati.net/Gachi/loading.html"))
 end
 
 timer.Create("fuckyou", 30, 0, fuckyou)
