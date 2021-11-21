4d605f0a6ca2d6a6075c45f7e01be9b18bfa159c
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-11-21 17:03:32 +0700

    delete mw2 and danger armory

--- addons/weps_arccw_mw2/lua/arccw/shared/attachments/mw2_integrallaser.lua
+++
@@ -1,9 +0,0 @@
-att.PrintName = "Hide Laser"
-att.Icon = Material("entities/acwatt_mw2_integrallaser.png", "smooth")
-att.Description = "What it says on the tin."
-att.Desc_Pros = {
-}
-att.Desc_Cons = {
-}
-att.AutoStats = true
-att.Slot = "mw2_hidelaser"
