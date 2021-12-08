4a6658785dab406b2477ddd6390e20cf749d8c31
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-12-09 01:37:21 +0700

    fix fonts downloading maybe

--- addons/core_panellib/lua/autorun/server/dlfonts.lua
+++ addons/core_panellib/lua/autorun/server/dlfonts.lua
@@ -1,4 +1,4 @@
 
-for k,v in pairs(file.Find("addons/panellib/resource/fonts/*.ttf", "GAME")) do 
+for k,v in pairs(file.Find("addons/core_panellib/resource/fonts/*.ttf", "GAME")) do 
 	resource.AddSingleFile("resource/fonts/" .. v)
 end
