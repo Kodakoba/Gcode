6c6be86ebc1429a32609603a3274b99de02276e5
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-01-19 00:45:19 +0700

    garbage tracking utils; manual GC

--- addons/core_just_lib_it_up/lua/lib_it_up/extensions/lua.lua
+++ addons/core_just_lib_it_up/lua/lib_it_up/extensions/lua.lua
@@ -85,3 +85,67 @@ function GenerateErrorer(err)
 
 	return errorers[err]
 end
+
+local cur = {}
+local buf = {}
+
+function GCMark(n)
+	if SERVER then return end
+
+	cur[n] = collectgarbage("count")
+end
+
+function GCPrint(n)
+	if SERVER then return end
+
+	local g = collectgarbage("count")
+
+	buf[n] = (buf[n] or 0) + g - (cur[n] or math.huge)
+	cur[n] = nil
+end
+
+hook.Add("HUDPaint", "GC_Trk", function()
+	if table.IsEmpty(buf) then return end
+
+	local sw, sh = ScrW(), ScrH()
+	local cnt = table.Count(buf)
+
+	local y = sh / 2 - cnt * 18
+
+	for k,v in SortedPairs(buf) do
+		local _, th = draw.SimpleText(k .. ": " .. ("%.2f"):format(v), "OS20",
+			sw - 4, y, color_white, 2)
+		y = y + th
+
+		buf[k] = nil
+	end
+end)
+
+if CLIENT then
+	collectgarbage("stop")
+
+	local MB = bit.lshift(1, 10)
+
+	local START, START_STEP = 1200 * MB, 120
+	local END, END_STEP = 1900 * MB, 250
+
+	local PANIC = jit.arch == "x64" and 3000 * MB or 1800 * MB
+
+	timer.Create("GC_Slow", 30, 0, function()
+		local cnt = math.min(collectgarbage("count"), END)
+		if cnt < START then return end -- not worth collecting
+
+		local stepSz = math.Remap(cnt, START, END, START_STEP, END_STEP)
+
+		collectgarbage("step", stepSz)
+	end)
+
+	timer.Create("GC_Panic", 1, 0, function()
+		local cnt = collectgarbage("count")
+		if cnt > PANIC then
+			print("!!! PANICKING GARBAGE COLLECTION !!!")
+			collectgarbage()
+			collectgarbage()
+		end
+	end)
+end
