cd97164507aaf0077b8a6a879827a8f4bf33e2aa
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-04-06 20:04:16 +0700

    nerf famas (35dmg @ 1000rpm -> 26dmg)

--- addons/weps_arccw_fas2/lua/weapons/arccw_mifl_fas2_famas.lua
+++ addons/weps_arccw_fas2/lua/weapons/arccw_mifl_fas2_famas.lua
@@ -25,8 +25,8 @@ SWEP.ViewModelFOV = 60
 
 SWEP.DefaultBodygroups = "000000000000"
 
-SWEP.Damage = 35
-SWEP.DamageMin = 24 -- damage done at maximum range
+SWEP.Damage = 26
+SWEP.DamageMin = 20 -- damage done at maximum range
 SWEP.Range = 75 -- in METRES
 SWEP.Penetration = 8
 SWEP.DamageType = DMG_BULLET
