af6b117972546e639a5a22e9a700e4172abc0329
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-04-09 18:53:41 +0700

    improve hitnumbers

--- addons/game_hit_numbers/lua/hitnums/cl_main.lua
+++ addons/game_hit_numbers/lua/hitnums/cl_main.lua
@@ -1,5 +1,7 @@
 HDN = HDN or {}
 HDN.Instances = HDN.Instances or muldim:new()
+HDN.NPInstances = HDN.NPInstances or muldim:new()
+
 HDN.Anim = Animatable("HDN")
 local anim = HDN.Anim
 
@@ -29,18 +31,19 @@ function HDN.AddIndicator(victim, dmg, crit, death, pos)
 		when = CurTime(),
 	}
 
-	local val, cur = HDN.Instances:Insert(entry, victim)
+	local insIn = IsPlayer(victim) and HDN.Instances or HDN.NPInstances
+	local val, cur = insIn:Insert(entry, victim)
 
-	local dat = HDN.Instances[victim]
+	local dat = insIn[victim]
 	dat.total = (dat.total or 0) + dmg
 	dat.totalAnim = dat.totalAnim or 0
 	dat.lastDmg = CurTime()
 	dat.firstDeal = dat.firstDeal or CurTime()
-	dat.pixvis = dat.pixvis or util.GetPixelVisibleHandle()
 	dat.death = death
+	dat.handle = util.GetPixelVisibleHandle()
 
 	local sameTime = 0
-	for k,v in ipairs(HDN.Instances[victim]) do
+	for k,v in ipairs(insIn[victim]) do
 		if v.when == entry.when and entry ~= v then
 			sameTime = sameTime + 1
 		end
@@ -120,11 +123,66 @@ local function vecToScreen(v)
 end
 
 local mx = Matrix()
+local col = Color(0, 0, 0)
 
-function HDN.DrawEntityNumber(ent, dat, state)
+local trIn, trOut = {}, {}
+
+local function doTrace(to)
+	trIn.filter = CachedLocalPlayer()
+	trIn.start = ep
+	trIn.endpos = to
+	trIn.output = trOut
+
+	return util.TraceLine(trIn)
+end
+
+function HDN.GetEntityVisPos(ent, dat, dopix)
+	local hnPos
+
+	local bone = ent:BoneToIndex("ValveBiped.Bip01_Head1")
+	local handle = dat.handle
+
+	if not bone then
+		hnPos = dat[1] and dat[1].pos
+			or ent:OBBCenter()
+
+		if dopix then
+			dat.vis = util.PixelVisible(hnPos, 12, handle)
+
+			if CurTime() - dat.firstDeal < 0.2 then
+				-- HOLY SHIT I HATE SOURCE
+				local tr = doTrace(hnPos)
+				local free = tr.Entity == ent or not tr.Hit
+				dat.vis = free and 1 or 0
+			end
+		end
+	else
+		local matrix = ent:GetBoneMatrix(bone)
+		hnPos = matrix:GetTranslation()
+
+		if dopix then
+			dat.vis = util.PixelVisible(hnPos, 12, handle) -- pixvis the bone pos, not above it
+
+			if CurTime() - dat.firstDeal < 0.2 then
+				-- HOLY SHIT I HATE SOURCE
+				local tr = doTrace(hnPos)
+				local free = tr.Entity == ent or not tr.Hit
+				dat.vis = free and 1 or 0
+			end
+		end
+
+		hnPos[3] = hnPos[3] + 8
+	end
+
+	return hnPos
+end
+
+function HDN.DrawEntityNumber(ent, dat, state, notfocus)
 	if not IsValid(ent) then return true end
 
-	local hnPos
+	local hnPos = HDN.GetEntityVisPos(ent, dat, true)
+	local pixvis = (notfocus and 0) or dat.vis or 0 -- util.PixelVisible(hnPos, 12, dat.pixvis)
+
 	--[[local attIdx = ent:LookupAttachment("eyes")
 
 	if attIdx then
@@ -157,40 +215,38 @@ function HDN.DrawEntityNumber(ent, dat, state)
 	anim:LerpColor(colMain, HDN.Colors[keyD], 0.2, 0, 0.3)
 	anim:LerpColor(colOut, HDN.Colors[keyO], 0.2, 0, 0.3)
 
-	local pixvis
-
-	if not hnPos then
-		local bone = ent:BoneToIndex("ValveBiped.Bip01_Head1")
-
-		if not bone then
-			hnPos = dat[1] and dat[1].pos
-				or ent:OBBCenter()
-
-			pixvis = util.PixelVisible(hnPos, 8, dat.pixvis)
-		else
-			local matrix = ent:GetBoneMatrix(bone)
-			hnPos = matrix:GetTranslation()
-			pixvis = util.PixelVisible(hnPos, 8, dat.pixvis) -- pixvis the bone pos, not above it
-
-			hnPos[3] = hnPos[3] + 8
-		end
-	end
-
 	local x, y
-	local do2d = false
+	local do2d = notfocus
 
 	local tx = ("-%d"):format(dat.totalAnim)
 	local txW, txH = surface.GetTextSizeQuick(tx, HDN.FontMain)
-	local scale = 0.5
+	local scale = notfocus and 0.4 or 0.5
+
+	local passedFirst = CurTime() - dat.firstDeal
+	local passed = CurTime() - dat.lastDmg
+	local appearFr = math.max(0, math.Remap(passedFirst, 0, 0.4, 0, 1))
+
+	appearFr = Ease(appearFr, 0.3)
+	local rotAng = -Lerp(appearFr, 90, 0)
+
+	if passed > HDN.DisappearTime - HDN.DisappearLen then
+		appearFr = math.Remap(passed, HDN.DisappearTime - HDN.DisappearLen, HDN.DisappearTime, 1, 0)
+		appearFr = Ease(appearFr, 0.3)
+		rotAng = -Lerp(appearFr, 60, 0)
+	end
 
 	--[[local bsz = 5
 	cam.Start3D()
 	render.DrawWireframeBox(hnPos, angle_zero, Vector(-bsz, -bsz, -bsz), Vector(bsz, bsz, bsz), color_white, false)
 	cam.End3D()]]
 
-	if pixvis > 0.5 then
+	local cy = state.cury or 0
+
+	if pixvis > 0.25 then
 		local vx, vy = vecToScreen(hnPos)
 		if not vx then return end
+
+		vx = vx - txW / 2 * scale
 
 		dat.lx, dat.ly = vx, vy
 
@@ -201,16 +257,17 @@ function HDN.DrawEntityNumber(ent, dat, state)
 		local fr = math.min(1, math.TimeFraction( 0, HDN.MoveLength, mpass ))
 		fr = Ease(fr, 0.25)
 
-		x = Lerp(fr, dat.sx, vx)
-		y = Lerp(fr, dat.sy, vy)
+		x = Lerp(fr, dat.sx or vx, vx)
+		y = Lerp(fr, dat.sy or vy, vy)
+
+		-- for animating cury when going back to 3d pos
+		state.cury = cy + txH * scale * (1 - fr) * appearFr * 0.875
 	else
-		local amt = state.amt or 0
-		state.amt = amt + 1
 
 		dat.moveIn = nil
 		dat.moved = true
 
-		dat.sx = ScrW() / 2 + txW / 2 + 16
+		dat.sx = ScrW() / 2 + 32
 		dat.sy = ScrH() / 2 + txH / 2 * scale
 
 		if dat.lx then
@@ -221,9 +278,11 @@ function HDN.DrawEntityNumber(ent, dat, state)
 			fr = Ease(fr, 0.25)
 
 			x = Lerp(fr, dat.lx, dat.sx)
-			y = Lerp(fr, dat.ly, dat.sy + amt * txH * scale * 0.875)
+			y = Lerp(fr, dat.ly, dat.sy + cy )
+			state.cury = cy + txH * scale * fr * appearFr * 0.875
 		else
-			x, y = dat.sx, dat.sy + amt * txH * scale * 0.875
+			x, y = dat.sx, dat.sy + cy
+			state.cury = cy + txH * scale * appearFr * 0.875
 		end
 
 		do2d = true
@@ -249,9 +308,6 @@ function HDN.DrawEntityNumber(ent, dat, state)
 		scale = scale * 0.6
 	end
 
-	local passedFirst = CurTime() - dat.firstDeal
-	local passed = CurTime() - dat.lastDmg
-
 	local shakeDur = is_last_crit and HDN.ShakeLength * 1.3 or HDN.ShakeLength
 
 	local shake = math.max(0, math.Remap(passed, 0, shakeDur, 1, 0)) * (is_last_crit and 12 or 4)
@@ -261,35 +317,25 @@ function HDN.DrawEntityNumber(ent, dat, state)
 	local shkX = math.cos(SysTime() * freq * 1.5) * shake / 2
 	local shkY = math.sin(SysTime() * freq) * shake
 
-	local rotFr = math.max(0, math.Remap(passedFirst, 0, 0.4, 0, 1))
-	rotFr = Ease(rotFr, 0.3)
-	local rotAng = -Lerp(rotFr, 90, 0)
-
-	if passed > HDN.DisappearTime - HDN.DisappearLen then
-		rotFr = math.Remap(passed, HDN.DisappearTime - HDN.DisappearLen, HDN.DisappearTime, 1, 0)
-		rotFr = Ease(rotFr, 0.3)
-		rotAng = -Lerp(rotFr, 60, 0)
-	end
-
 	mx:Reset()
 
 	mx:TranslateNumber(x + shkX, y + shkY)
 	mx:SetScaleNumber(scale, scale)
 
 	-- text is bottom-aligned so we add height
-	mx:TranslateNumber(txW * (1 - rotFr) / 2, txH * (1 - rotFr))
+	mx:TranslateNumber(0, -txH * (1 - appearFr))
 	mx:SetAnglesNumber(0, rotAng)
-	mx:TranslateNumber(0, 0)
+	mx:TranslateNumber(0, txH * (1 - appearFr))
 
 	tCols[1]:Set(colMain)
-	tCols[1].a = Lerp(rotFr, tCols[1].a * 0.33, tCols[1].a)
+	tCols[1].a = Lerp(appearFr, tCols[1].a * 0.33, tCols[1].a)
 
 	tCols[2]:Set(colOut)
-	tCols[2].a = Lerp(rotFr, tCols[2].a * 0.1, tCols[2].a)
+	tCols[2].a = Lerp(appearFr, tCols[2].a * 0.1, tCols[2].a)
 
 	DisableClipping(true)
 	cam.PushModelMatrix(mx)
-		draw.SimpleTextOutlined(tx, HDN.FontMain, 0, 0, tCols[1], 1, 4, 2, tCols[2])
+		draw.SimpleTextOutlined(tx, HDN.FontMain, 0, 0, tCols[1], 0, 4, 2, tCols[2])
 		White()
 		--surface.DrawOutlinedRect(-txW / 2, -txH, txW, txH, 2)
 	cam.PopModelMatrix()
@@ -310,11 +356,16 @@ hook.Add("HUDPaint", "HDN", function()
 		local rem = HDN.DrawEntityNumber(ent, dat, state)
 		if rem then HDN.Instances[ent] = nil end
 	end
+
+	for ent, dat in pairs(HDN.NPInstances) do
+		local rem = HDN.DrawEntityNumber(ent, dat, state, true)
+		if rem then HDN.NPInstances[ent] = nil end
+	end
 	draw.DisableFilters()
 end)
 
-hook.Add("PostDrawTranslucentRenderables", "HDN", function()
-	if not HDN.EnableSetting:GetValue() then return end
+hook.Add("PostDrawTranslucentRenderables", "HDN", function(a, b)
+	if not HDN.EnableSetting:GetValue() or a or b then return end
 
 	ep, ev = EyePos(), EyeVector()
 
