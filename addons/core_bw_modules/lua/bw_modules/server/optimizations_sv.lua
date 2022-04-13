f60f070c5b6460e8e9ca67fff2e5c7484c5b8797
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-04-13 15:55:12 +0700

    apply physics optimizations properly [?]

--- addons/core_bw_modules/lua/bw_modules/server/optimizations_sv.lua
+++ addons/core_bw_modules/lua/bw_modules/server/optimizations_sv.lua
@@ -1,10 +1,14 @@
-physenv.SetPerformanceSettings({
-    LookAheadTimeObjectsVsObject      =     0.15,
-    LookAheadTimeObjectsVsWorld       =     0.3,
-    MaxAngularVelocity                =  7272.7275390625,
-    MaxCollisionChecksPerTimestep     = 10000,
-    MaxCollisionsPerObjectPerTimestep =    3,
-    MaxFrictionMass                   =  2500,
-    MaxVelocity                       =  4000,
-    MinFrictionMass                   =    10
-})
+function BW.ApplyPhysSettings()
+	physenv.SetPerformanceSettings({
+	    LookAheadTimeObjectsVsObject      =     0.15,
+	    LookAheadTimeObjectsVsWorld       =     0.3,
+	    MaxAngularVelocity                =  7272.7275390625,
+	    MaxCollisionChecksPerTimestep     = 10000,
+	    MaxCollisionsPerObjectPerTimestep =    3,
+	    MaxFrictionMass                   =  2500,
+	    MaxVelocity                       =  4000,
+	    MinFrictionMass                   =    10
+	})
+end
+
+timer.Create("phys_shite_thx_garry", 30, 5, BW.ApplyPhysSettings)
